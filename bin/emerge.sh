9bd251ff39feeec1bf4311714231d650b2c4edc1
xluffy <xquang.foss@gmail.com>
2015-10-16 09:17:20 +0700

    nothing error

--- bin/emerge.sh
+++ bin/emerge.sh
@@ -4,7 +4,7 @@
 
 _base()
 {
-  local pkgs=(acpi ansible autoconf automake bc cmake cowsay cronie dos2unix dstat elinks flac fontconfig gawk gcc geany git go htop inxi jemalloc logrotate lsof lua lynx make mc nano ncdu openvpn pv rsync sed sys-apps/pciutils sys-apps/usbutils tmux unzip vim xzip zip)
+  local pkgs=(acpi ansible autoconf automake bc cmake cowsay cronie dos2unix dstat elinks flac fontconfig gawk gcc geany git go htop inxi jemalloc logrotate lsof lua lynx make app-misc/mc nano ncdu openvpn pv rsync sed sys-apps/pciutils sys-apps/usbutils tmux unzip vim xzip zip)
 
   for pkg in "${pkgs[@]}";do
     yes | emerge $pkg || break
