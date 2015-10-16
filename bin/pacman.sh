4d2395e74b6315a7aaf8054a4e608bd4c3538708
xluffy <xquang.foss@gmail.com>
2015-10-16 09:30:24 +0700

    script install lib/pkgs for arch system

--- bin/pacman.sh
+++ bin/pacman.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+# Descrip: Install lib and package after build Arch system
+# Author: @xluffy
 
 SUDO=$(which sudo)
 PM=$(which pacman)
 
-pks='ansible archey3 atop bridge-utils bzip2 cmake cowsay cronie curl dhclient dhcpcd dialog dmidecode dos2unix fabric firefox flac fontconfig gc gcc geany gzip hicolor-icon-theme htop httperf httpie keepassx lsof lua lxc lz4 lzo make mc meld mesa-libgl mime-types mkinitcpio mpd mpg123 \
-netctl ngrep ntfs-3g openvpn python python-setuptools ruby shotwell siege skype sshfs subversion supervisor tmux ttf-hanazono ttf-sazanami ttf-ubuntu-font-family unrar unzip vagrant virtualbox wireshark-cli wireshark-gtk xf86driproto xf86-input-evdev xf86-video-fbdev xf86-video-nouveau \
-xf86vidmodeproto xorg-appres xorg-bdftopcf xorg-fonts-alias xorg-fonts-encodings xorg-fonts-misc xorg-font-util xorg-font-utils xorg-iceauth xorg-mkfontdir xorg-mkfontscale xorg-server-common xorg-server-devel xorg-server-utils xorg-sessreg xorg-setxkbmap xorg-util-macros xorg-xauth xorg-xbacklight xorg-xcmsdb xorg-xgamma \
-xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xmodmap xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xscreensaver xz yaourt zathura zathura-djvu zathura-pdf-poppler zip'
+lib=(archey3 atop bridge-utils bzip2 cmake cowsay cronie curl dhclient dhcpcd dialog dmidecode dos2unix flac fontconfig gc gcc geany gzip htop lsof lua lxc lz4 lzo make meld mesa-libgl mime-types mkinitcpio mpg123 netctl ngrep ntfs-3g python python-setuptools ruby siege subversion supervisor tmux ttf-hanazono ttf-sazanami ttf-ubuntu-font-family unrar unzip xf86driproto xf86-input-evdev xf86-video-fbdev xf86-video-nouveau xf86vidmodeproto xorg-appres xorg-bdftopcf xorg-fonts-alias xorg-fonts-encodings xorg-fonts-misc xorg-font-util xorg-font-utils xorg-iceauth xorg-mkfontdir xorg-mkfontscale xorg-server-common xorg-server-devel xorg-server-utils xorg-sessreg xorg-setxkbmap xorg-util-macros xorg-xauth xorg-xbacklight xorg-xcmsdb xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbcomp xorg-xmodmap xorg-xrandr xorg-xrdb xorg-xrefresh xorg-xset xorg-xsetroot xz)
+pkg=(ansible cronie curl dhclient dhcpcd fabric firefox keepassx hicolor-icon-theme shotwell skype sshfs tmux openvpn vagrant virtualbox wireshark-cli wireshark-gtk xscreensaver yaourt zathura zathura-djvu zathura-pdf-poppler zip)
 
-for pk in ${pks[@]}; do
+for l in "${lib[@]}"; do
+  echo "Install lib is $l"
+  "$SUDO" "$PM" -S "$pk" --noconfirm || break
+
+for pk in "${pks[@]}"; do
 	echo "Install $pk ..."
-	$SUDO $PM -S "$pk" --noconfirm || continue
+	"$SUDO" "$PM" -S "$pk" --noconfirm || break
 done
-
-wget -O /usr/local/bin/git-cheat https://raw.githubusercontent.com/0xAX/git-cheat/master/git-cheat
-chmod 755 usr/local/bin/git-cheat
