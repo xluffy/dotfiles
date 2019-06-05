88411ca424958107a7d87a44ed321d49d87098db
xluffy <xquang.foss@gmail.com>
2019-06-05 19:56:49 +0700

    Delete unuse files, refactor brew.sh

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,45 +1,38 @@
 #!/bin/sh
 #
-# Setup mac
+# Purpose: Install brew on mac osx
+# Author: xluffy
 #
-sudo -v
 
-if [ ! $(which brew) ]; then
-  echo "Install brew ..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+DEBUG="${DEBUG:-true}"
 
-brew update
-brew upgrade --all
+[ "${DEBUG}" = "true"  ] && set -x
 
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+_log() {
+  printf "\x1B[2;32m"
+  echo "[LOG]" "[$(date +'%Y-%m-%d %H:%M:%S')]:" "$*"
+  printf "\x1B[0m"
+}
 
-brew install moreutils
-brew install findutils
-brew install gnu-sed --with-default-names
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
-echo "Adding the newly installed shell to the list of allowed shells"
-sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
-chsh -s /usr/local/bin/bash
+_die() {
+  printf "\x1B[2;31m"
+  echo "[ERROR]" "[$(date +'%Y-%m-%d %H:%M:%S')]:" "$*" >&2
+  exit 1 
+  printf "\x1B[0m"
+}
 
-brew install python
-# brew install ruby-build
-# brew install rbenv
-# LINE='eval "$(rbenv init -)"'
-# grep -q "$LINE" ~/.bash_profile || echo "$LINE" >> ~/.bash_profile
+_i_brew() {
+  if [ ! "$(command -v brew)" ]; then
+    echo "Install brew ..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
+  brew update
+  brew upgrade --all
+}
 
-brew install git
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install tree
+main() {
+  _i_brew || _die "Can't install brew"
+}
+
+main "$@"
