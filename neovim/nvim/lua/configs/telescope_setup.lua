e316ad4b663839c9afff7d0b0837625b332ea62e
xluffy <xquang.foss@gmail.com>
2021-11-30 10:35:56 +0700

    Remove neovim

--- neovim/nvim/lua/configs/telescope_setup.lua
+++
@@ -1,9 +0,0 @@
-local map = require('configs.utils').map
-
-local silent = { silent = true }
--- Navigate buffers and repos
-map('n', '<leader>fb', [[<cmd>Telescope buffers show_all_buffers=true theme=get_dropdown<cr>]], silent)
-map('n', '<leader>fe', [[<cmd>Telescope frecency theme=get_dropdown<cr>]], silent)
-map('n', '<leader>fs', [[<cmd>Telescope git_files theme=get_dropdown<cr>]], silent)
-map('n', '<leader>ff', [[<cmd>Telescope find_files <cr>]], silent)
-map('n', '<leader>fg', [[<cmd>Telescope live_grep theme=get_dropdown<cr>]], silent)
